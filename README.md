d679a1d5d4e7ca8c3a4f6f45833e0770e8f186cf
Sergey Grankin <sagran@gmail.com>
2012-01-20 11:51:20 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# About:
+
+ZoneBuilder automates generation of BIND9 zones for a domain, including:
+
+- creating zones for separate views (for split horizon setups)
+- creating in-addr.arpa and ip6.arpa zones
+
---
+++ example.com.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require './zonebuilder'
+
+PUBLIC_IP = '1.1.1.1'
+
+domain 'example.com' do
+  view :internal
+  view :external
+
+  soa 'ns1'
+
+  ip_external PUBLIC_IP
+  ip_internal '192.168.1.1'
+  ip6 '2001::1'
+
+  prefix_internal '192.168.1'
+  prefix6 '2001::/64'
+
+  mx 10, :mx1
+
+  ns_external 'ns1.example.org.'
+
+  host :* do
+    ip_external PUBLIC_IP
+  end
+
+  host :host1 do
+    ip_internal '192.168.1.1'
+    ip6 '2001::1'
+
+    name :vpn
+  end
+
+  host :host2 do
+    ip_internal '192.168.1.2'
+    ip6 '2001::2'
+
+    name :mail, :mx1, :www, :ns1
+  end
+end
